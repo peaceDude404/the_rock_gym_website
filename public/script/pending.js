a4a341d2d87331326184e8178ede9eaa3a73f8ca
peaceDude404 <lcs2019012@iiitl.ac.in>
2023-02-17 12:28:09 +0530

    uploading project to github

---
+++ public/script/pending.js
@@ -0,0 +1,4 @@
+var yc = Date.now().getYear(), mc = Date.now().getMonth(), dc = Date.now().getDate();
+var year = result.admission.getYear();
+console.log(year);
+document.getElementById("year").innerHTML = "sdfg";
